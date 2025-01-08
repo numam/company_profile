dbb9d3d886945898a28db61314517a14c52d3300
yusuf n <yusufnurm@gmail.com>
2025-01-08 18:23:14 +0700

    config primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'primary': "#1A1A1A",
+            }
         },
     },
 
