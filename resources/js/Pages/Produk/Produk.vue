72867f08cb40dd79c79c09dda5371331199a0bae
yusuf n <yusufnurm@gmail.com>
2025-01-09 22:49:55 +0700

    fix produk link detail

--- resources/js/Pages/Produk/Produk.vue
+++ resources/js/Pages/Produk/Produk.vue
@@ -17,9 +17,9 @@
           </button>
         </router-link>
       </div>
-      <img 
-        src="https://simpsonsmeats.com/cdn/shop/collections/Dry_Aged_and_Prime_Strip_Steak.jpg?v=1661459906&width=5325" 
-        alt="Delicious Meat Dishes" 
+      <img
+        src="https://simpsonsmeats.com/cdn/shop/collections/Dry_Aged_and_Prime_Strip_Steak.jpg?v=1661459906&width=5325"
+        alt="Delicious Meat Dishes"
         class="mt-4 lg:mt-0 ml-0 lg:ml-6 xl:ml-12 rounded shadow-lg w-full lg:w-3/5 xl:w-2/5 h-auto animate-zoom-in"
       />
     </div>
@@ -29,14 +29,16 @@
       <h2 class="text-xl lg:text-2xl font-semibold mb-4 text-center text-secondary animate-fade-in">Paling Populer</h2>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         <div v-for="(produk, index) in produkList" :key="index" class="bg-gray-900 shadow rounded p-4 lg:p-6 transform transition duration-500 hover:scale-105">
-          <img
-            :src="produk.image"
-            :alt="produk.title"
-            class="mb-4 transition-transform duration-300 hover:scale-110 w-full h-60 object-cover animate-slide-in-up"
-          />
-          <h3 class="font-bold text-lg text-white lg:text-xl animate-fade-in">{{ produk.title }}</h3>
-          <p class="text-secondary animate-fade-in">{{ produk.price }}</p>
-          <p class="text-gray-400 text-sm mt-2 line-clamp-2 animate-fade-in">{{ produk.description }}</p>
+            <Link :href="'/produk/' + produk.slug">
+                <img
+                    :src="produk.image"
+                    :alt="produk.title"
+                    class="mb-4 transition-transform duration-300 hover:scale-110 w-full h-60 object-cover animate-slide-in-up"
+                />
+                <h3 class="font-bold text-lg text-white lg:text-xl animate-fade-in">{{ produk.title }}</h3>
+                <p class="text-secondary animate-fade-in">{{ produk.price }}</p>
+                <p class="text-gray-400 text-sm mt-2 line-clamp-2 animate-fade-in">{{ produk.description }}</p>
+            </Link>
         </div>
       </div>
     </Link>
@@ -50,7 +52,7 @@
         </p>
         <button class="mt-4 px-4 py-2 lg:px-6 lg:py-3 bg-secondary text-black rounded-full hover:bg-primary hover:text-secondary transition duration-300">Beli Sekarang</button>
       </div>
-      <img 
+      <img
         src="https://images.squarespace-cdn.com/content/v1/59776eeef5e2314291c86837/1720550434867-9RBC01M2R92A9NVPIZ07/double+cut+chops.jpeg?format=1000w"
         alt="Doctor and Patient"
         class="mt-4 lg:mt-0 ml-0 lg:ml-9 rounded shadow-lg w-full lg:w-1/2 xl:w-1/3 h-auto animate-zoom-in"
@@ -68,6 +70,7 @@ import GuestLayout from '@/Layouts/GuestLayout.vue';
 const produkList = reactive([
   {
     id: 1,
+    slug: 'ribeye-steak',
     title: 'Ribeye Steak',
     image: 'https://images.squarespace-cdn.com/content/v1/54b852a3e4b0b0eaa0ff938e/1424136172939-11EO3F7E1Z9U71CG6UV5/Bison+Tenderlion.jpg',
     price: 'Rp. 20.000.000',
@@ -75,6 +78,7 @@ const produkList = reactive([
   },
   {
     id: 2,
+    slug: 'prime-rib',
     title: 'Prime Rib',
     image: 'https://oakavenuefarm.com/cdn/shop/collections/F47F11F4-EC03-4D10-91DC-7B8ED9522D0E.jpg?v=1729819820&width=2400',
     price: 'Rp. 13.000.000',
@@ -82,6 +86,7 @@ const produkList = reactive([
   },
   {
     id: 3,
+    slug: 'tomahawk-steak',
     title: 'Tomahawk Steak',
     image: 'https://oneworlddeli.com/cdn/shop/products/DingleyDellPorkTomahawk01_DSCF4613_2000x2000_45f9463a-2e8d-46fb-98bb-9ca5e4fd8961_460x@2x.jpg?v=1652410424',
     price: 'Rp. 10.000.000',
