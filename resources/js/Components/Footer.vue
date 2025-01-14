5b711a4a03e5f10539a2666bbea8307cea8f85d1
yusuf n <yusufnurm@gmail.com>
2025-01-14 15:01:48 +0700

    footer component

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="bg-primary text-gray-100 pt-24 pb-10">
+      <div class="container mx-auto text-center">
+        <p>&copy; 2025 D'MeatBoy | Semua Hak Dilindungi</p>
+        <div class="mt-4">
+          <a href="#" class="text-gray-400 hover:text-secondary mx-2 sm:mx-4">Instagram</a>
+          <a href="#" class="text-gray-400 hover:text-secondary mx-2 sm:mx-4">Facebook</a>
+          <a href="#" class="text-gray-400 hover:text-secondary mx-2 sm:mx-4">Twitter</a>
+        </div>
+      </div>
+    </footer>
+</template>
